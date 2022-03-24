d126f598858781536bb13c9128a4e8d59853caa5
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2022-03-24 09:06:40 +0100

    updates

--- EBASMakeCacheScratch.py
+++ EBASMakeCacheScratch.py
@@ -1,5 +1,6 @@
 def main():
     import matplotlib.pyplot as plt
+
     plt.close("all")
 
     from pyaerocom import const
@@ -13,11 +14,31 @@ def main():
     # print(od)
     DATA_ID = const.EBAS_MULTICOLUMN_NAME
     reader = pio.ReadUngridded(DATA_ID)
+    vars_to_cache = ["concCocpm25", "concso4", "SO4ugSm3"]
+    vars_to_cache = [
+        # "concso4pm25",
+        # "concso4pm10",
+        # "concso2",
+        # "concbc",
+        # "concoa",
+        # "concoc",
+        # "conctc",
+        # "concno2",
+        # "concno3",
+        # "concnh3",
+        # "concnh4",
+        # "wetso4",
+        # "concso4pr",
+    ]
+
     # data = reader.read(vars_to_retrieve="concCocpm25")
-    data = reader.read(vars_to_retrieve="concso4")
-    # data = reader.read(vars_to_retrieve="SO4ugSm3")
-    print(data)
+    for var_to_read in vars_to_cache:
+        data = reader.read(vars_to_retrieve=var_to_read)
+        print(f"# of unique stations: {len(data.unique_station_names)}")
+        print(data)
 
     # ETOPO1_Ice_g_gmt4.grd
+
+
 if __name__ == "__main__":
     main()
--- ReadEbasScratch.py
+++ ReadEbasScratch.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 
-DATA_ID = 'EBASMC'
+DATA_ID = "EBASMC"
 # VAR_NAME = 'concpm10'
 # VAR_NAME = 'conco3'
 # VAR_NAME = 'concso4'
-VAR_NAME = 'vmro3'
+VAR_NAME = "vmro3"
+
+
 def main():
     import pyaerocom.io as pio
-    obs_obj  = pio.ReadUngridded(DATA_ID)
+
+    obs_obj = pio.ReadUngridded(DATA_ID)
     obs_data = obs_obj.read(vars_to_retrieve=VAR_NAME)
     print(obs_data)
 
